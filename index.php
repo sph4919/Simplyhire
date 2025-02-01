38565354de73b05999d13956d8d2563e9c68b364
sph4919 <patelsugnaykumar492003@gmail.com>
2025-02-01 02:20:38 -0600

    add php database

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
+<?php
+
+
+$servername = "localhost";
+$username = "root";
+$password = "Sugnay@2003";
+$dbname = "server";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
