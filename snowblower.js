bfb8dd28e614bc4f45d557a8f6404f8534405fdf
sph4919 <patelsugnaykumar492003@gmail.com>
2025-02-01 13:56:36 -0600

    werew

--- snowblower.js
+++ snowblower.js
@@ -1,11 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('http://localhost:3000/services/snowblower')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json();
-        })
+    fetch('/services/snowblower')
+        .then(response => response.json())
         .then(data => {
             const container = document.querySelector('.container');
             container.innerHTML = ''; // Clear existing content
