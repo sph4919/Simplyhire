a05834147f1fab64a501b311cb82d494c2f4f814
sph4919 <patelsugnaykumar492003@gmail.com>
2025-02-01 04:14:01 -0600

    path module

--- js/server.js
+++ js/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const cors = require('cors'); // Import CORS
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.json());
