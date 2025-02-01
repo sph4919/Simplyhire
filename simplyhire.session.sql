914d4ad21c58fef8406f0a72cc22b2494b8faeab
sph4919 <patelsugnaykumar492003@gmail.com>
2025-02-01 03:47:28 -0600

    adding files

--- simplyhire.session.sql
+++ simplyhire.session.sql
@@ -1,11 +1,23 @@
-CREATE TABLE serviceprovider(
-    serviceid INT AUTO_INCREMENT PRIMARY KEY,
-    jobid INT,
-    lastname VARCHAR(255),
-    firstname VARCHAR(255),
-    servicetype VARCHAR(255),
-    currentcharges INT,
-    jobdescription TEXT
+
+CREATE TABLE user (
+    userid INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    job_id INT,
+    name VARCHAR(255)
 );
 
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Sugnay@2003';
+CREATE TABLE serviceagent (
+    service_id INT AUTO_INCREMENT PRIMARY KEY,
+    job_id INT,
+    name VARCHAR(255),
+    service_type VARCHAR(255),
+    current_charges INT,
+    job_description TEXT,
+    FOREIGN KEY (job_id) REFERENCES user(userid)
+);
+
+INSERT INTO user (userid, email, password, job_id, name) 
+VALUES (1, 'patelsugnay49@gmail.com', 'qwertyu', 1, 'sugnay');
+
+SELECT * FROM user;
