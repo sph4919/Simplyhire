48e986259396df22238fe8d6737a45db12684422
sph4919 <patelsugnaykumar492003@gmail.com>
2025-02-15 02:01:23 -0600

    adding erd diagram

--- js/providerlist.js
+++ js/providerlist.js
@@ -1,14 +1,4 @@
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     const serviceCards = document.querySelectorAll('.service-card');
-
-//     serviceCards.forEach(card => {
-//         card.addEventListener('click', function() {
-//             const serviceType = this.getAttribute('data-servicetype');
-//             window.location.href = `providerlist.html?servicetype=${serviceType}`;
-//         });
-//     });
-// });
 
 
 const list= document.querySelectorAll('.provider-card');
