4ceade6bc1b905ed9ca03e76554e3c0bb5c265e3
sph4919 <patelsugnaykumar492003@gmail.com>
2025-06-24 12:58:06 -0600

    fetching the main data table created

--- js/index.js
+++ js/index.js
@@ -3,43 +3,41 @@ let userlogin = document.getElementById("user-form");
 userlogin.addEventListener("submit",sendAutenReq);
 
 
-let UserEmail = document.getElementById("user-email");
-let UserPassword = document.getElementById("user-password");
 
+async function sendAutenReq(event)
+ {
+  event.preventDefault();
+  console.log("clicked");
+  let UserEmail = document.getElementById("user-email");
+  let UserPassword = document.getElementById("user-password");
+  let email = UserEmail.value;
+  let password = UserPassword.value;
 
-let email = UserEmail.value;
-let password = UserPassword.value;
+	 try 
+		 {
+      const res = await fetch(`http://localhost:3000/api/userLogin`,
+		     {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({email, password})
+          });
 
-
-async function sendAutenReq(email,password)
-        {
-			
-		     try 
-			   {
-                const res = await fetch('http://localhost:3000/api/userLogin',
-		        {
-                  method: 'POST',
-                  headers: { 'Content-Type': 'application/json' },
-                  body: JSON.stringify({email, password })
-                });
-
-                const result = await res.json();
-				console.log(result);
-                 if (res.status == 201)
-				  {
-                   alert("Login successful! with correct creds");
-				   console.log("Validation scuccessfull.");
-				   window.location.href = "../main.html";
-                  }
+       const result = await res.json();
+			 console.log(result);
+       if (res.status == 201)
+				{
+        alert("Login successful! with correct creds");
+			  console.log("Validation scuccessfull.");
+			  window.location.href = "../main.html";
+        }
 				else 
 				   {
 				   alert("Error: " + result.message + "Incorrect emial and password");
-                   }
-               } 
-			 catch (err) 
+           }
+       } 
+			  catch (err) 
 			   {
-                console.error("Fetch error:", err);
-                alert("Server error");
-               }
-			 
-        };
+          console.error("Fetch error:", err);
+          alert("Server error");
+         }
+};
--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', fetchServies);
+
+async function fetchServies()
+{
     const serviceCards = document.querySelectorAll('.service-card');
+
+
+  try
+   {
+      const res = await fetch(`http://localhost:3000/api/mainFetch`);
+      let data = res.json();
+      console.log(data);
+      const card = document.getElementById('card');
+      card.setAttribute("data-servicetype",data);
+      const name = document.getElementById('serviceName');
+      const description = document.getElementById('serviceDescription');
+    
+      name.value = data.something;
+      description.value = data.something;
+      
+
+   }
+  catch(err)
+   {
+    console.log(err);
+   } 
 
     serviceCards.forEach(card => {
         card.addEventListener('click', function() {
@@ -7,4 +31,4 @@ document.addEventListener('DOMContentLoaded', function() {
             window.location.href = `providerlist.html?servicetype=${serviceType}`;
         });
     });
-});
+};
